536ff150d31d5ca2a02e73800dd0c04391357521
y1guo <guoyi0328@gmail.com>
2023-05-16 17:47:57 -0700

    add pinyin search; add transcript pickle cache

--- launch.py
+++ launch.py
@@ -1,31 +1,52 @@
-import os, sys, shutil, time, json, ffmpeg
+import os, shutil, json, ffmpeg, pickle
 import gradio as gr
-import numpy as np
 import pandas as pd
-from utils import TRANSCRIPT_DIR, VOCAL_DIR, SLICE_DIR, FAVORITE_DIR, msg, get_duration
+from pypinyin import lazy_pinyin
+from utils import (
+    TRANSCRIPT_DIR,
+    VOCAL_DIR,
+    SLICE_DIR,
+    FAVORITE_DIR,
+    TMP_DIR,
+    msg,
+    get_duration,
+)
 
 
 MAX_SLICE_NUM = 7
 
 
-def load_transcript() -> tuple[pd.DataFrame, str]:
+def load_transcript(refresh: bool = False) -> tuple[pd.DataFrame, str]:
     msg("Search", "Loading Transcripts")
-    tmp = {k: [] for k in ["roomid", "basename", "start", "end", "text"]}
-    for file in sorted(os.listdir(TRANSCRIPT_DIR)):
-        if file.endswith(".json"):
-            with open(os.path.join(TRANSCRIPT_DIR, file)) as f:
-                base_name = os.path.splitext(file)[0]
-                roomid = base_name.split("_")[0]
-                for segment in json.load(f)["segments"]:
-                    tmp["roomid"].append(roomid)
-                    tmp["basename"].append(base_name)
-                    tmp["start"].append(segment["start"])
-                    tmp["end"].append(segment["end"])
-                    tmp["text"].append(segment["text"])
-    transcript = pd.DataFrame(tmp)
-    status = f"Loaded {len(transcript)} transcripts"
-    msg("Search", "Loading Transcripts Finished")
-    return transcript, status
+    try:
+        with open(os.path.join(TMP_DIR, "transcript.pkl"), "rb") as f:
+            transcript = pickle.load(f)
+            # return transcript, status
+    except FileNotFoundError:
+        tmp = {k: [] for k in ["roomid", "basename", "start", "end", "text", "pinyin"]}
+        for file in sorted(os.listdir(TRANSCRIPT_DIR)):
+            if file.endswith(".json"):
+                with open(os.path.join(TRANSCRIPT_DIR, file)) as f:
+                    base_name = os.path.splitext(file)[0]
+                    roomid = base_name.split("_")[0]
+                    for segment in json.load(f)["segments"]:
+                        tmp["roomid"].append(roomid)
+                        tmp["basename"].append(base_name)
+                        tmp["start"].append(segment["start"])
+                        tmp["end"].append(segment["end"])
+                        tmp["text"].append(segment["text"])
+                        tmp["pinyin"].append(" ".join(lazy_pinyin(segment["text"])))
+        transcript = pd.DataFrame(tmp)
+        with open(os.path.join(TMP_DIR, "transcript.pkl"), "wb") as f:
+            pickle.dump(transcript, f)
+    finally:
+        status = f"Loaded {len(transcript)} transcripts"
+        msg("Search", "Loading Transcripts Finished")
+        return transcript, status
+
+
+def refresh_transcript() -> tuple[pd.DataFrame, str]:
+    return load_transcript(refresh=True)
 
 
 def trim(vocal: str, start: float, end: float, slice: str):
@@ -42,21 +63,34 @@ def trim(vocal: str, start: float, end: float, slice: str):
     )
 
 
-def search(transcript: pd.DataFrame, roomid: str, keyword: str, page: int = 1):
+def search(
+    transcript: pd.DataFrame,
+    roomid: str,
+    keyword: str,
+    options: list[str],
+    page: int = 1,
+):
     labels = []
     slices = []
     processes = []
     # filter transcript by roomid
     if roomid != "all":
         transcript = transcript[transcript["roomid"] == roomid]
-    # filter transcript by keyword
-    transcript = transcript[transcript["text"].str.contains(keyword)].reset_index(
-        drop=True
-    )
+    # filter transcript by keywords, allow multiple keywords separated by space
+    for k in keyword.split():
+        if "Pinyin" in options:
+            transcript = transcript[
+                transcript["pinyin"].str.contains(" ".join(lazy_pinyin(k)))
+            ]
+        else:
+            transcript = transcript[transcript["text"].str.contains(k)]
+    transcript = transcript.reset_index(drop=True)
     # regulate page number
     total_page = (len(transcript) - 1) // MAX_SLICE_NUM + 1
     page = max(1, min(page, total_page))
     for i in range((page - 1) * MAX_SLICE_NUM, len(transcript)):
+        if len(slices) >= MAX_SLICE_NUM:
+            break
         row = transcript.iloc[i]
         # trim vocal
         vocal = os.path.join(VOCAL_DIR, f"{row['basename']}.mp3")
@@ -66,13 +100,15 @@ def search(transcript: pd.DataFrame, roomid: str, keyword: str, page: int = 1):
             SLICE_DIR,
             f"{row['basename']}_{row['start']:.0f}_{row['end']:.0f}_{row['text']}.mp3",
         )
-        if len(slices) < MAX_SLICE_NUM:
+        labels.append(f"# [{row['roomid']}] {row['text']}")
+        if "Audio" in options:
             processes.append(trim(vocal, start, end, slice))
             msg("Search", "Trimming", file=slice)
-            labels.append(f"# [{row['roomid']}] {row['text']}")
+            slices.append(slice)
+        elif os.path.exists(slice):
             slices.append(slice)
         else:
-            break
+            slices.append(None)
     # wait for all processes to finish
     for i, process in enumerate(processes):
         if not process:
@@ -94,20 +130,28 @@ def search(transcript: pd.DataFrame, roomid: str, keyword: str, page: int = 1):
     return page, total_page, *labels, *slices
 
 
-def prev_page(transcript: pd.DataFrame, roomid: str, keyword: str, page: int):
-    return search(transcript, roomid, keyword, page - 1)
+def prev_page(
+    transcript: pd.DataFrame, roomid: str, keyword: str, options: list[str], page: int
+):
+    return search(transcript, roomid, keyword, options, page - 1)
 
 
-def next_page(transcript: pd.DataFrame, roomid: str, keyword: str, page: int):
-    return search(transcript, roomid, keyword, page + 1)
+def next_page(
+    transcript: pd.DataFrame, roomid: str, keyword: str, options: list[str], page: int
+):
+    return search(transcript, roomid, keyword, options, page + 1)
 
 
-def cache_pages(transcript: pd.DataFrame, roomid: str, keyword: str) -> None:
+def cache_pages(
+    transcript: pd.DataFrame, roomid: str, keyword: str, options: list[str]
+) -> None:
+    if "Audio" not in options:
+        options.append("Audio")
     msg("Search", "Caching All Slices", "This may take a while")
-    _, total_page, *_ = search(transcript, roomid, keyword, 1)
+    _, total_page, *_ = search(transcript, roomid, keyword, options, 1)
     msg("Search", "Cached", f"Page 1 of {total_page}")
     for page in range(2, total_page + 1):
-        search(transcript, roomid, keyword, page)
+        search(transcript, roomid, keyword, options, page)
         msg("Search", "Cached", f"Page {page} of {total_page}")
 
 
@@ -137,7 +181,7 @@ if __name__ == "__main__":
         gr.Markdown("""# <center>Auto-Transcribe</center>""")
         with gr.Row():
             with gr.Column():
-                reload = gr.Button(value="Reload Transcripts")
+                refresh = gr.Button(value="Refresh Transcripts")
                 cache = gr.Button(value="Cache All Slices")
                 status = gr.Textbox(
                     value=init_status, label="Status", interactive=False
@@ -146,6 +190,11 @@ if __name__ == "__main__":
                     choices=["all"] + sorted(df["roomid"].unique().tolist()),
                     label="Room ID",
                     value="all",
+                )
+                options = gr.CheckboxGroup(
+                    choices=["Audio", "Pinyin"],
+                    value=["Audio"],
+                    label="Options",
                 )
                 keyword = gr.Textbox(value="晚上好", label="Search For ... (Press Enter)")
             with gr.Column(scale=100):
@@ -171,52 +220,33 @@ if __name__ == "__main__":
 
         keyword.submit(
             search,
-            [
-                transcript,
-                roomid,
-                keyword,
-            ],
+            [transcript, roomid, keyword, options],
             [page, total_page, *labels, *slices],
         )
 
         page.submit(
             search,
-            [
-                transcript,
-                roomid,
-                keyword,
-                page,
-            ],
+            [transcript, roomid, keyword, options, page],
             [page, total_page, *labels, *slices],
         )
 
         backward.click(
             prev_page,
-            [
-                transcript,
-                roomid,
-                keyword,
-                page,
-            ],
+            [transcript, roomid, keyword, options, page],
             [page, total_page, *labels, *slices],
         )
 
         forward.click(
             next_page,
-            [
-                transcript,
-                roomid,
-                keyword,
-                page,
-            ],
+            [transcript, roomid, keyword, options, page],
             [page, total_page, *labels, *slices],
         )
 
         for i in range(MAX_SLICE_NUM):
             favorite[i].click(save_to_favorite, slices[i], status)
 
-        cache.click(cache_pages, [transcript, roomid, keyword])
+        cache.click(cache_pages, [transcript, roomid, keyword, options])
 
-        reload.click(load_transcript, outputs=[transcript, status])
+        refresh.click(refresh_transcript, outputs=[transcript, status])
 
-    app.launch()
+    app.launch(share=False)
